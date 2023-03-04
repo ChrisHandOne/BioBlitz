d305fc46d42dbcd054b23ece76be5d671b304227
Chris Hand <cj.hand@gmail.com>
2023-03-04 13:03:49 -0800

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Chris Hand on 2023.03.04.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
