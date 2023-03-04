decdc94a36bcaa28f013ba60e34c969bd61f6caa
Chris Hand <cj.hand@gmail.com>
2023-03-04 15:29:27 -0800

    Randomize the game board

--- BioBlitz/GameBoard.swift
+++ BioBlitz/GameBoard.swift
@@ -24,10 +24,35 @@ class GameBoard: ObservableObject {
             
             for col in 0..<columnCount {
                 let bacteria = Bacteria(row: row, col: col)
+                
+                if row <= rowCount / 2 {
+                    if row == 0 && col == 0 {
+                        bacteria.direction = .north
+                    } else if row == 0 && col == 1 {
+                        bacteria.direction = .east
+                    } else if row == 1 && col == 0 {
+                        bacteria.direction = .south
+                    } else {
+                        bacteria.direction = Bacteria.Direction.allCases.randomElement()!
+                    }
+                } else {
+                    if let counterpart = getBacteria(atRow: rowCount-1-row, col: columnCount - 1 - col) {
+                        bacteria.direction = counterpart.direction.opposite
+                    }
+                }
+                               
                 newRow.append(bacteria)
             }
             
             grid.append(newRow)
+        }
+        
+        func getBacteria(atRow row: Int, col: Int) -> Bacteria? {
+            guard row >= 0 else { return nil }
+            guard row < grid.count else { return nil }
+            guard col >= 0 else { return nil }
+            guard col < grid[0].count else { return nil }
+            return grid[row][col]
         }
         
         grid[0][0].color = .green
